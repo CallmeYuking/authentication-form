79d2cc74718dc714bc1119b79dbfc11414fd7df7
Erica <ericaquan.edu@gmail.com>
2020-03-03 22:12:58 +0000

    adding login routes. render login form and login logic.

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-var express =require("express"),
-	mongoose = require("mongoose")
+var express 				= require("express"),
+	mongoose 				= require("mongoose"),
+	passport 				= require("passport"),
+	bodyParser				= require("body-parser"),
+	User 					= require("./models/user"),
+	LocalStrategy 			= require("passport-local"),
+	passportLocalMongoose 	= require("passport-local-mongoose")
+	
 
-var app = express()
+mongoose.connect("mongodb://localhost/auth_demo_app", {useNewUrlParser: true, useUnifiedTopology: true});
+
+var app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(require("express-session")({
+	secret: "This is a secret!",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session())
+
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//===============================
+//ROUTES
+//===============================
 
 app.get("/", function(req, res){
 	res.render("home")
@@ -11,6 +35,48 @@ app.get("/", function(req, res){
 app.get("/secret", function(req, res){
 	res.render("secret")
 });
+
+//===============================
+//AUTH ROUTES
+//===============================
+
+//show sign up form
+app.get("/register", function(req, res){
+	res.render("register")
+})
+
+//handing user sign up
+app.post("/register", function(req, res){
+	var username = req.body.username;
+	var password = req.body.password;
+	User.register(new User({username: username}), password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register")
+		} else {
+			passport.authenticate("local")(req, res, function(){
+				res.redirect("/secret")
+			})
+		}
+	})
+})
+
+// LONGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+	res.render("login")
+})
+
+//login logic
+app.post("/login", passport.authenticate("local", {
+	successRedirect: "/secret",
+	failureRedirect: "/login"
+}), function(req, res){
+});
+
+
+
+
 
 
 app.listen(8000, function(){
